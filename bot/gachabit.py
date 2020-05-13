1a41a43dcc3070088af79177132ad06575715eab
John C <edibleleadpaint@gmail.com>
2020-05-12 23:54:19 -0400

    cleaned up code a bit

--- bot/gachabit.py
+++ bot/gachabit.py
@@ -16,43 +16,40 @@ logger = logging.getLogger()
 
 import requests
 
-# handles twitter stuff 
 import tweepy 
-#from config import create_twitter_api
 import config
-
 
 import json
 
 
 sleep_timer = 15
+keywords =  ["test"]
+
 
 ###
 # Scan for all new mentions 
 ###
-def check_mentions(api, keywords):
+def check_mentions(api):
 	logger.info("retrieving mentions")
 	check_time = datetime.now()
 	mentions = tweepy.Cursor(api.mentions_timeline).items()
 	for tweet in mentions:
 		try:
+			# Only check tweets that haven't previously been checked
 			tweet_timestamp = tweet.created_at
 			if last_check is not None:
 				if tweet_timestamp < last_check:
 					continue
-			if tweet.in_reply_to_status_id is not None:
-				continue
-			# check for matching keyword (disable to reply to all mentions)
-			if any(keyword in tweet.text.lower() for keyword in keywords):
+			
+			if validate_mention(tweet):
 				try:
 					username = tweet.user.screen_name
 					userid = tweet.user.id
-					compose_tweet(api, tweet, userid, username)
+					compose_reply(api, tweet, userid, username)
 				except AttributeError as e:
 					username = None
 					userid = None
 					logger.error("Failed to obtain user - {}".format(e))
-
 
 		except Exception as e:
 			logger.error("Failed while fetching mentions - {}".format(e))
@@ -60,10 +57,36 @@ def check_mentions(api, keywords):
 			
 	return check_time
 
+
+###
+# Check that the given tweet is valid to reply to 
+###
+def validate_mention(tweet):
+	valid = True
+	# TODO - Make sure tweet has not already been replied to
+
+	# Make sure tweet is not a reply to someone else
+	if tweet.in_reply_to_status_id is not None:
+		valid = False
+
+	# TODO - Make sure user has not already received a reply in the recent past
+
+	# Check for matching keyword (disable to reply to all mentions)
+	check_keywords = False
+	if check_keywords:
+		has_keywords = False
+		if any(keyword in tweet.text.lower() for keyword in keywords):
+			has_keywords = True
+		if has_keywords is False:
+			valid = False
+
+	return valid
+
+
 ###
 # Compose the reply
 ###
-def compose_tweet(api, src_tweet, userid, username):
+def compose_reply(api, src_tweet, userid, username):
 	if not username:
 		logger.info("invalid username, tweet abandoned")
 		return
@@ -99,17 +122,7 @@ def shutdown():
 	return
 
 ###
-# Main bot loop, periodically checks for new mentions and replies to them
-###
-def reply_cycle():
-	last_check = check_mentions(api, ["test"])
-	logger.info("replied to tweets up to %s" % last_check)
-	config.save_cfg()
-	logger.info("napping...zzz")
-	sleep(sleep_timer)
-
-###
-# initialize global variables
+# Initialize global variables
 ###
 def init():
 	global api 
@@ -119,12 +132,20 @@ def init():
 	last_check = None
 
 ###
-# main 
+# Main bot loop, periodically checks for new mentions and replies to them.
 ###
 def main():
 	init()
 	while True:
-		reply_cycle()
+		last_check = check_mentions(api)
+		logger.info("Replied to tweets up to %s" % last_check)
+		config.save_cfg()
+		logger.info("Napping...zzz")
+		sleep(sleep_timer)
+
+
+
+
 
 if __name__ == "__main__":
 	main()
